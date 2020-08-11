929badcf525cebf5e9f02fcef41b010b1e23f0e6
Mohit <mohit@mediccreations.com>
2020-08-11 20:13:01 +0530

    unit test for login

--- app/screens/__test__/Login.test.js
+++ app/screens/__test__/Login.test.js
@@ -7,14 +7,42 @@ import createSagaMiddleware from 'redux-saga';
 import Login from '../Login';
 import { ExTextInput, ExButton } from '../../components';
 
-describe('Songs', () => {
+describe('Login', () => {
+  const dispatch = jest.fn();
+  redux.useDispatch.mockReturnValue(dispatch);
+  const navigation = {
+    navigate: jest.fn(),
+  };
   describe('UI test cases', () => {
-    const wrapper = shallow(<Login />);
+    const sagaMiddleware = createSagaMiddleware();
+    const mockStore = configureStore([sagaMiddleware]);
+    const store = mockStore({
+      banner: true,
+      user: {},
+    });
+
+    jest
+      .spyOn(redux, 'useSelector')
+      .mockImplementationOnce(() => store.getState().banner)
+      .mockImplementationOnce(() => store.getState().user);
+
+    const wrapper = shallow(<Login navigation={navigation} />);
     it('renders the main view', () => {
       expect(wrapper).toMatchSnapshot();
       expect(wrapper.find(SafeAreaView).length).toEqual(1);
       expect(wrapper.find(ExTextInput).length).toEqual(2);
       expect(wrapper.find(ExButton).length).toEqual(1);
+    });
+    it('Simulate TextInput and Button', () => {
+      const textInput = wrapper.find(ExTextInput).first();
+      textInput.simulate('changeText', 'test4@mediccreations.com');
+
+      const passowrdtextInput = wrapper.find(ExTextInput).at(1);
+      passowrdtextInput.simulate('changeText', 'Medic123@');
+
+      const button = wrapper.find(ExButton);
+      button.simulate('press');
+      expect(button).toBeTruthy();
     });
   });
 });
